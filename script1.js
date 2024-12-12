c380d1ee18e82fc1550f56156950d036a4b9567d
mamta-kri06 <153258193+mamta-kri06@users.noreply.github.com>
2024-12-12 21:43:30 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -35,3 +35,44 @@ function loadFavoriteLocations() {
     favoriteLocationsList.appendChild(listItem);
   });
 }
+
+const suggestionsList = document.getElementById("suggestions");
+
+function getSearchSuggestions() {
+  const apiKey = "664b1d004cadbf77bfe12998bb540dcc";
+  const query = userLocation.value.trim();
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${apiKey}`;
+
+  fetch(url)
+    .then((response) => {
+      // console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      // console.log(data);
+      displaySuggestions(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching location suggestions:", error.message);
+    });
+}
+
+function displaySuggestions(suggestions) {
+  suggestionsList.innerHTML = "";
+  if (suggestions.length === 0) {
+    suggestionsList.innerHTML = `<li class="list-group-item">No matches found</li>`;
+    return;
+  }
+
+  suggestions.forEach((location) => {
+    const suggestionItem = document.createElement("li");
+    suggestionItem.classList.add("list-group-item");
+    suggestionItem.textContent = `${location.name}, ${location.country}`;
+    suggestionItem.style.cursor = "pointer";
+    suggestionItem.onclick = () => {
+      userLocation.value = location.name;
+      suggestionsList.innerHTML = "";
+    };
+    suggestionsList.appendChild(suggestionItem);
+  });
+}
